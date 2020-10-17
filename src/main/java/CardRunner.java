b1ab04f4ee3463fd8fcd972101ca290ec10942aa
John Travlos <660936@aacps.org>
2020-10-17 00:21:47 +0000

    Added constuctor and methods to the TerrapinCard followed the istruction on class room

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,29 @@
 public class CardRunner {
-    public static void main(String args[]){
-        // ☕
-        System.out.println("hey");
+  public static void main(String args[]){
+    // ☕
+    TerrapinCard billy = new TerrapinCard(20);
+       
+    TerrapinCard brian = new TerrapinCard(30); 
+   
+    brian.payEconomical();
+    System.out.println("Brian: " + brian.getBalance());
 
-    }
+        
+    billy.payGourment();
+    System.out.println("Billy: "+ billy.getBalance());
+
+    brian.payGourment();
+    System.out.println("Brian: " +   brian.getBalance());  
+  
+    billy.loadMoney(20);
+    System.out.println("Billy: "+ billy.getBalance());
+  
+    brian.loadMoney(50);
+    System.out.println("Brian: " +   brian.getBalance()); 
+
+    billy.payEconomical();
+    billy.payEconomical();
+    System.out.println("Billy: " +   billy.getBalance()); 
+  }
 }
+
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,8 +1,34 @@
 public class TerrapinCard {
   private double balance;
 
+  
   public TerrapinCard(double balance){
-    this.balnace = balance;
+    this.balance = balance;
   }
 
+  
+  public void payEconomical(){
+    if(this.balance >= 2.5){
+      this.balance -= 2.5;
+    }
+  }
+  
+
+  public void payGourment(){
+    if(this.balance >= 4){
+      this.balance -= 4;
+    }
+  }
+
+  public void loadMoney(double amount){
+    if(amount >= 0){
+      this.balance += amount;
+    }else{
+      this.balance += (amount * -1);
+    }
+  }
+
+  public double getBalance(){
+    return this.balance;
+  }
 }
